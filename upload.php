5dd01599ab52ff73d3416ca282e4850a46510638
kevinychen <binonunquineist@gmail.com>
2014-02-19 16:52:44 -0500

    Add option on how much to squish the PDF.

--- upload.php
+++ upload.php
@@ -9,31 +9,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 \includepdf[nup = %s, landscape=%s, delta=%s %s, pages={1-}]{uploaded.pdf}
 \end{document}
 ';
+    if (!preg_match('/[01](\.[02468]?)/', $_POST['delta'])) {
+        array_push($errors, 'Invalid vertical squish amount parameter');
+    } else {
+        $delta = floatval($_POST['delta']);
+    }
     if ($_POST['nup'] === '2on1') {
         $nup = '1x2';
         $landscape = 'true';
+        $vert_delta = '0';
+        $horiz_delta = '-' . strval($delta) . 'in';
     } else if ($_POST['nup'] === '4on1') {
         $nup = '2x2';
         $landscape = 'false';
-    } else {
-        array_push($errors, 'Invalid number of pages per sheet parameter');
+        $vert_delta = '-' . strval($delta / 2) . 'in';
+        $horiz_delta = '-' . strval($delta / 2) . 'in';
     }
-    if (!preg_match('/[01](\.[0-9]?)/', $_POST['vert_delta'])) {
-        array_push($errors, 'Invalid vertical squish amount parameter');
-    } else if ($_POST['vert_delta'] === '0') {
-        $vert_delta = '0';
-    } else {
-        $vert_delta = '-' + $_POST['vert_delta'] + 'in';
-    }
-    if (!preg_match('/[01](\.[0-9]?)/', $_POST['horiz_delta'])) {
-        array_push($errors, 'Invalid horizontal squish amount parameter');
-    } else if ($_POST['horiz_delta'] === '0') {
-        $horiz_delta = '0';
-    } else {
-        $horiz_delta = '-' + $_POST['horiz_delta'] + 'in';
-    }
-    $vert_delta = '0';
-    $horiz_delta = '-1in';
     $latex = sprintf($format, $nup, $landscape, $vert_delta, $horiz_delta);
     file_put_contents($code . '.tex', $latex);
 
@@ -51,9 +42,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <h1>Squish Print</h1>
     <form method="post" action="/squishprint/upload.php" enctype="multipart/form-data">
         <p>Upload a PDF: <input type="file" name="file"></p>
-        <p>How much to squish?</p>
-        <input type="radio" name="nup" value="2on1">2 pages per sheet<br/>
+        <input type="radio" name="nup" value="2on1" checked>2 pages per sheet<br/>
         <input type="radio" name="nup" value="4on1">4 pages per sheet<br/>
+        Loose<input type="range" name="delta" min="0" max="1.99" step="0.2" value="1">Squished</br>
         <p><input type="submit" value="submit"></p>
     </form>
     </body>
